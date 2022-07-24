04d16d21f514c024d11819bfebb394911756b769
Nikhil Kr. Soni <nikhil@uniacco.com>
2022-07-25 03:28:50 +0530

    Rename heroku app

--- store/settings.py
+++ store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-j_(_mlq^vwq^b=%tc=k)(pwc_-36nobg(r#t#8fs=+(&cs@i4f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'anakin-store.herokuapp.com']
 
 
 # Application definition
